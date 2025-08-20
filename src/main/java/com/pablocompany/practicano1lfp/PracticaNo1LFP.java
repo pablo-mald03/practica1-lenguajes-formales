7a66e5dae7278fa4e43f64b0a5ea8f9a6826c605
Pablo_Maldonado <pablomaldonadodev@gmail.com>
2025-08-20 16:16:15 -0600

    Se ha inicializado el repositorio

---
+++ src/main/java/com/pablocompany/practicano1lfp/PracticaNo1LFP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pablocompany.practicano1lfp;
+
+/**
+ *
+ * @author pablo
+ */
+public class PracticaNo1LFP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
